c966ab202b6ca6ff72c7f63f315a034bb812f87d
altabored <alegutier.021@gmail.com>
2024-06-05 10:41:08 -0600

    AL FIN

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    text = 'Oh... ¡Hola, mundo! Esto es una referencia al que tenia Netflix en producción. Me quiero morir, las ganas de llorar son increibles, y más sabiendo que esto sigue fallando.'
+    text = 'Oh... ¡Hola, mundo! Esto es una referencia al que tenia Netflix en producción. Y como costó hacer la referencia, aquí está.'
     return text
 
 if __name__ == '__main__':
